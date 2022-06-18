407655b04bd16df14acca372eccdd178e36fbaf1
Princeprem76 <princeprem76@gmail.com>
2022-06-18 15:18:28 +0545

    product serial change

--- product/serializers.py
+++ product/serializers.py
@@ -6,20 +6,17 @@ from product.models import products, category, subcategory
 class subCategoryName(serializers.ModelSerializer):
     class Meta:
         model = subcategory
-        fields = '__all__'
+        fields = ['sub_category_name']
 
 
 class allCategoryName(serializers.ModelSerializer):
-    sub_category = subCategoryName
-
     class Meta:
         model = category
         fields = ['id', 'category_name', 'sub_category']
 
 
 class allProductName(serializers.ModelSerializer):
-    product_category = allCategoryName
-
+    product_category = allCategoryName()
     class Meta:
         model = products
         fields = ['id', 'product_name', 'product_image', 'product_price', 'product_category', 'get_discounted_price',
